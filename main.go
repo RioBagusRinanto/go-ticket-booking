3d2ec4deb84378afa6ce8dfd633b0bec23595dba
riobagusrinanto <riobagusrinanto@gmail.com>
2025-12-25 23:18:11 +0700

    initialize gin framework for golang project

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
 func main() {
 
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.POST("/login", func(ctx *gin.Context) {
+		var json Login
+		if err := ctx.ShouldBind(&json); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.User != "lucario" || json.Password != "123" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "login successful"})
+
+	})
+	router.Run() // listen and serve on
 }
